0c3e9e5196718081dca4c5bb2a71c15dfbe38582
Bharat <bharatappguru@gmail.com>
2015-12-08 17:38:34 +0530

    Facebook Integration using Swift 2 Tutorial

---
+++ FBIntegrationWithSwift/Views/ViewController/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  FBIntegrationWithSwift
+//
+//  Created by TheAppGuruz-iOS-103 on 17/11/15.
+//  Copyright © 2015 TheAppGuruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate
+{
+    
+    @IBOutlet var btnFacebook: FBSDKLoginButton!
+    @IBOutlet var ivUserProfileImage: UIImageView!
+    @IBOutlet var lblName: UILabel!
+
+//    MARK: ViewLifeCycle Methods
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        configureFacebook()
+    }
+    
+//    MARK: FBSDKLoginButtonDelegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+    {
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            
+            let strFirstName: String = (result.objectForKey("first_name") as? String)!
+            let strLastName: String = (result.objectForKey("last_name") as? String)!
+            let strPictureURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
+            
+            self.lblName.text = "Welcome, \(strFirstName) \(strLastName)"
+            self.ivUserProfileImage.image = UIImage(data: NSData(contentsOfURL: NSURL(string: strPictureURL)!)!)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        
+        ivUserProfileImage.image = nil
+        lblName.text = ""
+    }
+    
+//    MARK: Other Methods
+    
+    func configureFacebook()
+    {
+        btnFacebook.readPermissions = ["public_profile", "email", "user_friends"];
+        btnFacebook.delegate = self
+    }
+    
+}
